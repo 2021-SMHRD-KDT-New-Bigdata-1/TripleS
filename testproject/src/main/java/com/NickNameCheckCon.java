d082b43d89d18ee0ec7499bdf8e082ffc58b10aa
LeeKilHwan <dlrlfghks94@gmail.com>
2021-09-24 15:53:16 +0900

    이메일, 닉네임 중복체크

---
+++ testproject/src/main/java/com/NickNameCheckCon.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.DAO.MemberDAO;
+
+@WebServlet("/NickNameCheckCon")
+public class NickNameCheckCon extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+String data = request.getParameter("nickname");
+		
+		MemberDAO dao = new MemberDAO();
+		boolean check = dao.nicknameCheck(data);
+
+		PrintWriter out= response.getWriter();
+		
+		out.print(check);
+
+	}
+
+}
